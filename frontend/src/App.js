6590864757419ac1558717f7e01b253b69250586
Aru Gyani <gyaniaru@gmail.com>
2021-04-14 03:51:49 -0500

    created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 
 import Home from './views/Home'
-import Navbar from './components/Navbar'
+import About from './views/About'
+import Login from './views/Login'
+
+import NavigationBar from './components/NavigationBar'
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Home/>
-    </div>
+    <Router>
+      <div>
+        <NavigationBar/>
+        
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/login" component={Login}/> 
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+import '../styles/NavigationBar.css'
+
+function NavigationBar(props) {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+            <NavbarBrand className="logo" href="/">desitrak</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink className="link" href="/about">About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="link" href="/login">Login</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavigationBar;
